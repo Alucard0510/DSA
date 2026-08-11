64800c68d691b8eec884695dd8f813b860e681be
Alucard <harsh.pdy10@gmail.com>
2026-08-11 23:51:06 +0530

    Enhance LinkedListBasic with additional node operations
    Added functions to count, sum, find max, and search nodes in the linked list.

--- Linked List/LinkedListBasic.cpp
+++ Linked List/LinkedListBasic.cpp
@@ -16,6 +16,50 @@ void display(struct node * node){
         cout << node->data << " ";
         node = node->next;
     }
+    cout << endl;
+}
+
+void countNodes(struct node* node){
+    int count = 0;
+    while (node != NULL) {
+        count++;
+        node = node->next;
+    }
+    cout << "Total number of nodes is: " << count << endl;
+}
+
+void sumNodes(struct node* node){
+    int sum = 0;
+    while (node != NULL) {
+        sum += node->data;
+        node = node->next;
+    }
+    cout << "Sum of data of all node is: " << sum << endl;
+}
+
+void maxNodes(struct node* node){
+    int max = node->data;
+    while (node != NULL) {
+        if ((node->data) > max) {
+            max = node->data;
+        }
+        node = node->next;
+        
+    }
+    cout << "Max value from all nodes is: " << max << endl;
+}
+
+int searchNode(struct node* node, int target){
+    int index = 0;
+    while (node != NULL) {
+        index++;
+        if ((node->data) == target) {
+            return index;
+        }
+        node = node->next;
+    }
+    return -1;
+    
 }
 
 int main () {
@@ -31,5 +75,17 @@ int main () {
     fourth->next = fifth;
 
     display(head);
+
+    countNodes(head);
+
+    sumNodes(head);
+
+    maxNodes(head);
+
+    int target;
+    cout << "Enter value to be found: ";
+    cin >> target;
+    int result = searchNode(head, target);
+    cout << target << " is at node: " << result << endl;
     return 0;
 }
